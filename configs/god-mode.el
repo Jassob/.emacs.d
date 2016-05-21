df593c51328acacfbbc5b0c002439aad7724f787
Jacob Jonsson <jassob@live.se>
2016-05-21 20:26:49 +0200

    Added god mode config

---
+++ configs/god-mode.el
@@ -0,0 +1,10 @@
+;;; ============================================================================
+;;; Configuration file for God mode.
+;;;
+;;; Author: Jassob
+;;; ============================================================================
+
+(use-package god-mode
+  :ensure t
+  :config
+  (global-set-key (kbd "<escape>") 'god-local-mode))
