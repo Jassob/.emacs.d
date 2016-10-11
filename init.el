fa660806a43716b0de1b6e490f9ccb80cfc03e1d
Jacob Jonsson <jassob@live.se>
2016-10-11 17:11:57 +0200

    Moved a bunch of stuff around.
    An attempt to make my init.el easier to overview

--- init.el
+++ init.el
@@ -1,16 +1,25 @@
 ;;; ============================================================================
 ;;; Initialize ELPA (Emacs Lisp Package Archive)
 ;;; ============================================================================
-(require 'package) ;; You might already have this line
+(require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
 ;;; ============================================================================
-;;; General stuff
+;;; EXWM
+;;; ============================================================================
+(require 'exwm)
+(require 'exwm-config)
+(exwm-config-default)
+
+;;; ============================================================================
+;;; Keybindings
+;;; ============================================================================
+(global-set-key (kbd "s-u") 'revert-buffer)
+
+;;; ============================================================================
+;;; Hooks
 ;;; ============================================================================
 (defun contextual-menubar (&optional frame)
   "Never show menubar, toolbar or scrollbar."
@@ -20,16 +29,51 @@
   (set-frame-parameter frame 'vertical-scroll-bars nil))
 
 (add-hook 'after-make-frame-functions 'contextual-menubar)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
 
+;;; ============================================================================
+;;; General stuff
+;;; ============================================================================
 (when (display-graphic-p)
   (menu-bar-mode -1)
   (tool-bar-mode -1)
   (toggle-scroll-bar -1))
 
+;; Display clock in modeline
+(display-time)
+(column-number-mode)
+(blink-cursor-mode 0)
+
+;; Set a bunch of stuff
+(setq initial-scratch-message ""
+      inhibit-startup-message t
+
+      ;; Disable visual or graphic error bell
+      ring-bell-function 'ignore
+
+      ;; Unset right option key as meta
+      ns-right-alternate-modifier nil
+
+      ;; Light-theme is not activated
+      light-theme-activated nil
+
+      ;; don't litter my fs tree (save backups in /tmp)
+      backup-directory-alist `((".*" . ,temporary-file-directory))
+      auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
+      auto-save-list-file-prefix temporary-file-directory
+      backup-by-copying t
+
+      ;; Keep at most 4 copies of every file (2 new and then 2 old copies)
+      kept-new-versions 2
+      kept-old-versions 2
+      delete-old-versions t
+
+      ;; use versioned backups
+      version-control t)
+
+;; Theming
 (load (concat user-emacs-directory "sundown.el"))
-
-(setq light-theme-activated nil)
-
 (defun dark-or-light-theme (lighttheme darktheme)
   "Checks whether the sun has set and loads the darktheme in that case,
    otherwise it loads the lighttheme instead."
@@ -52,54 +96,11 @@
                 'sanityinc-tomorrow-day
                 'sanityinc-tomorrow-night)
 
-;; Display clock in modeline
-(display-time)
-;; Display current column in modeline
-(column-number-mode)
-;; Don't blink the cursor
-(blink-cursor-mode 0)
-;; Disable menubar
-(menu-bar-mode -1)
-;; Disable visual or graphic error bell
-(setq ring-bell-function 'ignore)
-
-(require 'exwm)
-(require 'exwm-config)
-(exwm-config-default)
-
 ;; Don't prompt as much
 (fset 'yes-or-no 'y-or-n)
-(remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
-
-;; Don't use messages that you don't read
-(setq initial-scratch-message "")
-(setq inhibit-startup-message t)
-
-;; Always delete trailing whitespace
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; Use auto indent mode
 (electric-indent-mode t)
-
-;; Unset right option key as meta
-(setq ns-right-alternate-modifier nil)
-
-;; Revert buffer with S-u
-(global-set-key (kbd "s-u") 'revert-buffer)
-
-;; Backups
-(setq backup-by-copying t ; don't clobber symlinks
-
-      ;; don't litter my fs tree
-      backup-directory-alist '(("." . "~/.saves"))
-
-      ;; Keep at most 4 copies of every file (2 new and then 2 old copies)
-      kept-new-versions 2
-      kept-old-versions 2
-      delete-old-versions t
-
-      ;; use versioned backups
-      version-control t)
 
 ;;; ============================================================================
 ;;; Load separate config files
